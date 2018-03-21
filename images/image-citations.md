334e2873453da8bcdca12a1a23f2af3227b47782
rob <robert.osterburg@gmail.com>
2018-03-21 09:42:29 -0600

    adding start of python intro pynb-based notebook

---
+++ images/image-citations.md
@@ -0,0 +1,16 @@
+# Images
+
+1.  **The Good**
+    1.  ![WALL-E](./WALL-Eposter.jpg)
+    1.  Cite: WALL-E. (2018, March 14). Retrieved March 16, 2018, from <https://en.wikipedia.org/wiki/WALL-E>
+
+___
+2.  **The Bad**
+    1.  ![HAL9000](./HAL9000_Case.svg.png)
+    2.  Cite: File:HAL9000 Case.svg. (n.d.). Retrieved March 16, 2018, from <https://commons.wikimedia.org/wiki/File:HAL9000_Case.svg>
+By Grafiker61 (Own work) [CC BY-SA 4.0] (https://creativecommons.org/licenses/by-sa/4.0)
+
+___
+3.  **The Ugly**
+    1.  ![Terminator Genisys](./terminator.jpg)
+    2.  Cite: Springer, A. (2015, November 20). Terminator Genisys: Time Is Not On My Side. Retrieved March 16, 2018, from <https://www.slugmag.com/dvd-reviews/terminator-genisys-time-is-not-on-my-side/>
---
+++ notebooks/Week1-Python-Intro.py
@@ -0,0 +1,90 @@
+def intro():
+
+
+    # slide
+    '''
+    # Why python?
+
+    * General purpose programming language
+        - Strenghts in data science and web development
+
+    * Efficient syntax designed to be read by people
+
+    * Gradual learning curve supports beginners and advanced users
+
+    * An interpreted language
+        - Code is compiled at runtime --> Hence reputation for being slow
+        - Bindings to C for libraries like Numpy --> Result in high performance
+        - What is more valuable? Developer time or machine time?
+        - If the later, analyze and optimize bottlenecks
+
+    * Friendly and inclusive community of developers
+    '''
+
+    # slide
+    '''
+    # Why python revisited?
+
+    ![Language popularity among data scientists](../images/language-popularity-in-data-science.png)
+
+    Cite: [O'Reilly 2017 Data Science Salary Survey](http://www.oreilly.com/data/free/2017-data-science-salary-survey.csp)
+    '''
+
+    # slide
+    '''
+    # Language popularity trends
+
+    ![General purpose vs domain specific language popularity](../images/language-popularity.png)
+
+    Cite: [Stack Overflow Trends](https://insights.stackoverflow.com/trends?utm_source=so-owned&utm_medium=blog&utm_campaign=trends&utm_content=blog-link&tags=r%2Cstatistics%2Cpython%2Cjava%2Cc%2B%2B)
+    '''
+
+    # slide
+    '''
+    # The short history of Python
+
+    * Created by Guido van Rossum in the late 1980s
+
+    * Named for _Monty Python Flying Circus_ British TV show from the 1970's
+
+    * Versions
+        - 1994 version 1
+        - 2000 version 2
+        - 2008 version 3
+        - 2016 version 3.6
+        - 2018 version 3.7 (expected)
+
+    * Python 2.7 end-of-life is Jan 1, 2020
+            > Let's not play games with semantics. The way I see the situation for 2.7
+            > is that EOL is January 1st, 2020, and there will be no updates, not even
+            > source-only security patches, after that date. Support (from the core devs,
+            > the PSF, and python.org) stops completely on that date.
+            > _Guido van Rossum_, Python BDFL
+
+    * **We will use Python 3 in this course**
+
+    Cite: Rossum, G. V. [Python-Dev] Python 2.7 -- bugfix or security before EOL? Retrieved March 21, 2018, from https://mail.python.org/pipermail/python-dev/2018-March/152348.html
+    '''
+
+    # slide
+    '''
+    # Environment
+
+    * Anaconda Python is recommended
+        - Why Anaconda?  Most data science package are included.  You are ready to go with minimal setup.
+        - Download the **Python 3.latest version** for your OS from [https://www.anaconda.com/distribution/](https://www.anaconda.com/distribution/)
+        - Alternatively, if you are comfortable with Python, you can install all the same tools using pip.  
+
+    '''
+
+def variable():
+    # slide
+    '''
+    # Data types
+
+    ## Numbers
+    '''
+
+    2 + 2
+    '''
+    '''
